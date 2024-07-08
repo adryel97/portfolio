6b085aa0c25245ac5dbcc54f426d41e65f875286
adryel97 <adryel.giovaniny@gmail.com>
2024-07-08 14:36:42 -0300

    start

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '15px',
+      screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1500px",
+      },
+  },
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["dark"],
+  },
+}
+
